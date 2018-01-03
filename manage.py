cb6673cefa6d5c996dc641dc69f95ffe498b2a7d
Jeramee <jerameeflemming@gmail.com>
2018-01-03 17:54:14 -0500

    R1-reverting and updating locations page

--- manage.py
+++ manage.py
@@ -6,8 +6,6 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "frikadell.settings")
     try:
         from django.core.management import execute_from_command_line
-        reload(sys)  
-        sys.setdefaultencoding('utf8')
     except ImportError:
         # The above import may fail for some other reason. Ensure that the
         # issue is really that Django is missing to avoid masking other
